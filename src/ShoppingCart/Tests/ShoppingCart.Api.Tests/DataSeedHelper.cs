ddcb373ee71bb22fdd4c9e33339f1e8008f9f767
Лазаренко Дмитрий <d.lazarenko@solarl.ru>
2022-11-29 11:15:27 +0300

    +api test

---
+++ src/ShoppingCart/Tests/ShoppingCart.Api.Tests/DataSeedHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bogus;
+using ShoppingCart.DataAccess;
+using ShoppingCart.Domain;
+
+namespace ShoppingCart.Api.Tests
+{
+    public static class DataSeedHelper
+    {
+        public static void InitializeDbForTests(ShoppingCartContext context)
+        {
+            var productFaker = new Faker<Product>()
+                .RuleFor(x => x.Id, Guid.NewGuid)
+                .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(x => x.Price, f => f.Commerce.Random.Decimal(10, 1000));
+
+            var products = productFaker.Generate(10);
+
+            context.Set<Product>().AddRange(products);
+            context.SaveChanges();
+        }
+    }
+}
